50143940969e1178c1384e64c53261839409f326
Dmitriy <baturovdmitriy@gmail.com>
2019-05-27 22:55:43 +0700

    Вынес реализацию поиска удаленных,новых, измененных страниц в отдельный класс

--- src/java_code/Main.java
+++ src/java_code/Main.java
@@ -1,9 +1,7 @@
 package java_code;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Main {
 
@@ -12,54 +10,25 @@ public class Main {
         t_old.put("s1/1", "p123");
         t_old.put("s1/2", "p2");
         t_old.put("s2/1", "p11");
-        t_old.put("s3/1", "p22"); //lost
+        t_old.put("s2/2", "p2222"); //lost
+        t_old.put("s2/3", "p24124");
+        t_old.put("s3/1", "p22");   //lost
+        t_old.put("s3/3", "p532");
 
         Map<String, String> t_new = new HashMap<>();
-        t_new.put("s1/1", "p1");
+        t_new.put("s1/1", "p1");    //modified
         t_new.put("s1/2", "p2");
         t_new.put("s2/1", "p1111"); //modified
-        t_new.put("s3/2", "p33"); //new
+        t_new.put("s2/3", "p24124");
+        t_new.put("s2/4", "p24231");//new
+        t_new.put("s3/2", "p33");   //new
+        t_new.put("s3/3", "p532");
 
-        //ищем исчезнувшие страницы
-        //добавляем текущие url-ы в set.
-        final Set<String> Urls = new HashSet<>(t_new.keySet());
-        //добавляем в тот же set вчерашние url-ы
-        Urls.addAll(t_old.keySet());
-        System.out.println("all: " + Urls);
-        Set<String> newUrls, oldUrls;
-//        newUrls = Urls;
-//        oldUrls = Urls;
-        newUrls = new HashSet<>(Urls);
-        oldUrls = new HashSet<>(Urls);
 
-        newUrls.removeAll(t_old.keySet());
-        System.out.println("newUrls: " + newUrls);
-        //после удаления элементов из newUrls обновляется и Urls! Так не должно быть! Чего я не понимаю???
-        //ладно если бы я обновил Urls, тогда обновилась бы и newUrls, но наоборот то как...
-        //понял...
-        /*было так:
-         * newUrls = Urls;
-         * oldUrls = Urls;
-         * изменил на:
-         * newUrls = new HashSet<>(Urls);
-         * oldUrls = new HashSet<>(Urls);
-         */
-
-        oldUrls.removeAll(t_new.keySet());
-        System.out.println("oldUrls: " + oldUrls);
-
-        //осталось найти изменившиеся страницы
-        Set<String> sameUrls = new HashSet<>(Urls);
-        //из всех url-ов удаляем уникальные, которые есть только в новой или старой таблице
-        sameUrls.removeAll(newUrls);
-        sameUrls.removeAll(oldUrls);
-        System.out.println("sameUrls: " + sameUrls);//получили общие url-ы
-        //сраниваем по общему ключу значения из старой и новой таблицы, если не равны, выводим
-        for (String s : sameUrls) {
-            if (!t_new.get(s).equals(t_old.get(s))) {
-                System.out.println("изменилась страница с url: " + s);
-            }
-        }
+        SearchUrl searchUrl = new SearchUrl(t_new, t_old);
+        System.out.println("getLostUrls: " + searchUrl.getLostUrls());
+        System.out.println("getNewUrls: " + searchUrl.getNewUrls());
+        System.out.println("getModifiedUrls: " + searchUrl.getModifiedUrls());
 
     }
 }
